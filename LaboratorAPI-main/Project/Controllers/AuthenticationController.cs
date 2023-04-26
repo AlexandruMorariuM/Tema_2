768f4d253621d5d0d565d86fa36f70d708f01784
Morariu Alexandru <93342935+AlexandruMorariuM@users.noreply.github.com>
2023-04-26 19:40:12 +0300

    Authentication Controller

---
+++ LaboratorAPI-main/Project/Controllers/AuthenticationController.cs
@@ -0,0 +1,52 @@
+﻿using DataLayer.Entities;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using DataLayer.Dtos;
+using Core.Services;
+
+namespace Project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        public static User user = new User();
+
+        private MyAuthenticationService authenticationService { get; set; }
+        public AuthenticationController(MyAuthenticationService authenticationService)
+        {
+            this.authenticationService = authenticationService;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<User>> Register(UserDto request)
+        {
+            authenticationService.CreatePasswordHash(request.Password, out byte[] passwordHash, out byte[] passwordSalt);
+
+            user.Id = (int)request.Id;
+            user.Username = request.Username;
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+            user.Role = request.Role;
+
+            return Ok(user);
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<string>> Login(LoginDto request)
+        {
+            if (user.Username != request.Username)
+            {
+                return BadRequest("User not found.");
+            }
+
+            if (!authenticationService.VerifyPasswordHash(request.Password, user.PasswordHash, user.PasswordSalt))
+            {
+                return BadRequest("Wrong password");
+            }
+
+            string token = authenticationService.CreateToken(user);
+            return Ok(token);
+        }
+    }
+}
